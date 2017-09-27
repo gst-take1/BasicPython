710f248dfb7b53a14058dd7d638590721582c9bb
gst-task1 <garima.shivani@gmail.com>
2017-09-27 16:27:46 -0400

    Adding csvfile - read/write operations

--- arrays.py
+++ arrays.py
@@ -4,3 +4,11 @@ for i in arr:
 
 for idx,val in enumerate(arr):
     print 'In enumerate idx :' + `idx` + ' Value: ' + `val`
+
+if len(arr) > 6:
+    print("Array %s is a big array of %d" % ('arr', len(arr)));
+
+if len(arr) > 6 and len(arr) <= 10:
+    print('Array %(array)s is a medium array of length %(len)d' % {"array": 'arr', "len":len(arr)});
+
+print "arr[1:5]", arr[1:5]

7c929d2420c1485c6bdebf99182d6ab3ea9978d9
gst-take1 <garima.shivani@gmail.com>
2017-10-09 11:48:57 -0400

    basic Numpy stuff

---
+++ basicNumpy/basic1.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+# Create a 3-d array
+y = np.zeros((2,3,4))
+
+print(y)
+print('#################################################')
+# Create a 3-d array
+y1 = np.zeros((2,3,2))
+
+print(y1)
